ba81ec81823e0724e189d3d8e549bb894649e1cb
qb0C80aE <qb0C80aE@gmail.com>
2016-12-20 15:43:59 +0900

    Implement controller methods to get data as text

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -51,6 +51,35 @@ func processSingleGet(c *gin.Context, model interface{}, actualLogic func(*gorm.
 	}
 }
 
+func processSingleGetText(c *gin.Context, model interface{}, actualLogic func(*gorm.DB, string, string) (string, error)) {
+	ver, err := version.New(c)
+
+	if err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+
+	id := c.Params.ByName("id")
+	db := dbpkg.DBInstance(c)
+	db = dbpkg.SetPreloads(c.Query("preloads"), db)
+	fields := helper.ParseFields(c.DefaultQuery("fields", "*"))
+	queryFields := helper.QueryFields(model, fields)
+
+	result, err := actualLogic(db, id, queryFields)
+	if err != nil {
+		content := gin.H{"error": "item with id#" + id + " not found"}
+		c.JSON(404, content)
+		return
+	}
+
+	if version.Range("1.0.0", "<=", ver) && version.Range(ver, "<", "2.0.0") {
+		// conditional branch by version.
+		// 1.0.0 <= this version < 2.0.0 !!
+	}
+
+	c.String(200, result)
+}
+
 func processMultiGet(c *gin.Context, model interface{}, actualLogic func(*gorm.DB, string) ([]interface{}, error)) {
 	ver, err := version.New(c)
 
@@ -114,6 +143,35 @@ func processMultiGet(c *gin.Context, model interface{}, actualLogic func(*gorm.D
 			c.JSON(200, fieldMaps)
 		}
 	}
+}
+
+func processMultiGetText(c *gin.Context, model interface{}, actualLogic func(*gorm.DB, string) (string, error)) {
+	ver, err := version.New(c)
+
+	if err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+
+	db := dbpkg.DBInstance(c)
+	db = dbpkg.SetPreloads(c.Query("preloads"), db)
+	db = dbpkg.SortRecords(c.Query("sort"), db)
+	db = dbpkg.FilterFields(c, model, db)
+	fields := helper.ParseFields(c.DefaultQuery("fields", "*"))
+	queryFields := helper.QueryFields(model, fields)
+
+	result, err := actualLogic(db, queryFields)
+	if err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+
+	if version.Range("1.0.0", "<=", ver) && version.Range(ver, "<", "2.0.0") {
+		// conditional branch by version.
+		// 1.0.0 <= this version < 2.0.0 !!
+	}
+
+	c.String(200, result)
 }
 
 func processCreate(c *gin.Context, container interface{}, model interface{}, actualLogic func(*gorm.DB, interface{}) (interface{}, error)) {
