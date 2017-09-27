cd5542d209e93d4dca8a4f5f7c5262c02cda56e2
qb0C80aE <qb0C80aE@gmail.com>
2017-09-27 11:46:15 +0900

    Move cascading deletion logic into triggers

--- models/template_persistent_parameter.go
+++ models/template_persistent_parameter.go
@@ -2,13 +2,14 @@ package models
 
 import (
 	"database/sql"
+	"github.com/jinzhu/gorm"
 	"github.com/qb0C80aE/clay/extensions"
 )
 
 // TemplatePersistentParameter is the model class what represents model-independent parameters used in templates
 type TemplatePersistentParameter struct {
 	ID          int             `json:"id" gorm:"primary_key;AUTO_INCREMENT"`
-	TemplateID  int             `json:"template_id" gorm:"unique_index:template_id_name" sql:"type:integer references templates(id) on delete cascade"`
+	TemplateID  int             `json:"template_id" gorm:"unique_index:template_id_name" sql:"type:integer references templates(id)"`
 	Name        string          `json:"name" gorm:"unique_index:template_id_name"`
 	ValueInt    sql.NullInt64   `json:"value_int"`
 	ValueFloat  sql.NullFloat64 `json:"value_float"`
@@ -28,6 +29,19 @@ func SharedTemplatePersistentParameterModel() *TemplatePersistentParameter {
 	return sharedTemplatePersistentParameterModel
 }
 
+// SetupInitialData setups the initial data
+func (templatePersistentParameter *TemplatePersistentParameter) SetupInitialData(db *gorm.DB) error {
+	db.Exec(`
+		create trigger if not exists DeleteTemplatePersistentParameterAfterTemplateDeletion after delete on templates
+		begin
+			delete from template_persistent_parameters where template_id = old.id;
+		end;
+	`)
+
+	return nil
+}
+
 func init() {
+	extensions.RegisterInitialDataLoader(sharedTemplatePersistentParameterModel)
 	extensions.RegisterModel(sharedTemplatePersistentParameterModel)
 }
