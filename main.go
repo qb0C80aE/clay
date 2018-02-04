177ecb5616cd7977d933295a6a370eb8191f44da
qb0C80aE <qb0C80aE@gmail.com>
2018-02-04 15:18:33 +0900

    Add files to run on heroku

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"github.com/qb0C80aE/clay/cmd"
+	_ "github.com/qb0C80aE/loam"    // Install Loam module by importing
+	_ "github.com/qb0C80aE/pottery" // Install Pottery module by importing
 )
 
 func main() {
