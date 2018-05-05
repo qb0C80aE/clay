94dc49712d341cd8f7368bc44e764276bf0aa5be
qb0C80aE <qb0C80aE@gmail.com>
2018-05-05 18:26:32 +0900

    Rename EphemeralText to EphemeralTemplate in order to make it easier to tell what's for

---
+++ controller/ephemeral_template_generation.go
@@ -0,0 +1,50 @@
+package controller
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/qb0C80aE/clay/extension"
+	"github.com/qb0C80aE/clay/logging"
+	"github.com/qb0C80aE/clay/model"
+)
+
+type ephemeralTemplateGenerationController struct {
+	BaseController
+}
+
+func newEphemeralTemplateGenerationController() *ephemeralTemplateGenerationController {
+	return CreateController(&ephemeralTemplateGenerationController{}, model.NewEphemeralTemplateGeneration()).(*ephemeralTemplateGenerationController)
+}
+
+func (receiver *ephemeralTemplateGenerationController) GetResourceSingleURL() (string, error) {
+	modelKey, err := receiver.model.GetModelKey(receiver.model, "")
+	if err != nil {
+		logging.Logger().Debug(err.Error())
+		return "", err
+	}
+
+	ephemeralTemplateResourceName, err := extension.GetAssociatedResourceNameWithModel(model.NewEphemeralTemplate())
+	if err != nil {
+		logging.Logger().Debug(err.Error())
+		return "", err
+	}
+
+	return fmt.Sprintf("%s/:%s/generation", ephemeralTemplateResourceName, modelKey.KeyParameter), nil
+}
+
+func (receiver *ephemeralTemplateGenerationController) GetRouteMap() map[int]map[int]gin.HandlerFunc {
+	routeMap := map[int]map[int]gin.HandlerFunc{
+		extension.MethodGet: {
+			extension.URLSingle: receiver.GetSingle,
+		},
+	}
+	return routeMap
+}
+
+func (receiver *ephemeralTemplateGenerationController) OutputGetSingle(c *gin.Context, code int, result interface{}, fields map[string]interface{}) {
+	OutputTextWithType(c, code, result)
+}
+
+func init() {
+	extension.RegisterController(newEphemeralTemplateGenerationController())
+}
---
+++ controller/ephemeral_template_raw.go
@@ -0,0 +1,50 @@
+package controller
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/qb0C80aE/clay/extension"
+	"github.com/qb0C80aE/clay/logging"
+	"github.com/qb0C80aE/clay/model"
+)
+
+type ephemeralTemplateRawController struct {
+	BaseController
+}
+
+func newEphemeralTemplateRawController() *ephemeralTemplateRawController {
+	return CreateController(&ephemeralTemplateRawController{}, model.NewEphemeralTemplateRaw()).(*ephemeralTemplateRawController)
+}
+
+func (receiver *ephemeralTemplateRawController) GetResourceSingleURL() (string, error) {
+	modelKey, err := receiver.model.GetModelKey(receiver.model, "")
+	if err != nil {
+		logging.Logger().Debug(err.Error())
+		return "", err
+	}
+
+	ephemeralTemplateResourceName, err := extension.GetAssociatedResourceNameWithModel(model.NewEphemeralTemplate())
+	if err != nil {
+		logging.Logger().Debug(err.Error())
+		return "", err
+	}
+
+	return fmt.Sprintf("%s/:%s/raw", ephemeralTemplateResourceName, modelKey.KeyParameter), nil
+}
+
+func (receiver *ephemeralTemplateRawController) GetRouteMap() map[int]map[int]gin.HandlerFunc {
+	routeMap := map[int]map[int]gin.HandlerFunc{
+		extension.MethodGet: {
+			extension.URLSingle: receiver.GetSingle,
+		},
+	}
+	return routeMap
+}
+
+func (receiver *ephemeralTemplateRawController) OutputGetSingle(c *gin.Context, code int, result interface{}, fields map[string]interface{}) {
+	OutputTextWithType(c, code, result)
+}
+
+func init() {
+	extension.RegisterController(newEphemeralTemplateRawController())
+}
